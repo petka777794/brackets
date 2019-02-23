02f26d85cd718144e920d2e3f166c3f3fd566778
d_kolodko <d.kolodko@aisnovations.com>
2019-02-23 17:32:53 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  debugger;
+  var stack = [];    
+
+  for (let j = 0; j < str.length; j++) {
+    const symbol = str[j];
+    
+    let flag = false;
+    
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      const configElement = bracketsConfig[i];
+
+      const stackElement = stack[stack.length - 1];
+
+      if(stackElement == configElement[0] && symbol == configElement[1]){
+        stack.pop();
+        flag = true;
+      }
+    }
+    
+    if (!flag) stack.push(symbol);
+  }    
+  if (stack.length == 0 ) return true;
+  
+  return false;  
 }
